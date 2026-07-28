9b96765f072320c8a6d38293d87faef3b5a4d113
efanwilliamsfoell-art <efanwilliamsfoell@gmail.com>
2026-07-28 22:02:59 +0100

    Update app.py.py

--- app.py.py
+++ app.py.py
@@ -85,6 +85,7 @@ with col1: # stremlit layout
 
         stl.success(f"Muons remaining at target distance: {n0[-1]:.1f}")
 
+pip install plotly[express]
 import plotly.express as px
 
 fig = px.line(
