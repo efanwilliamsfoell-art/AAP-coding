3dae5eac8694647aa1ce534a774b6614a2af055b
efanwilliamsfoell-art <efanwilliamsfoell@gmail.com>
2026-07-28 13:22:11 +0100

    Update app.py.py

--- app.py.py
+++ app.py.py
@@ -65,5 +65,5 @@ if stl.button("Calculate Surviving Muons"):
     X.append(x)
     n0.append(N0)
 
-stl.success(f"Muons remaining at target distance: 'n0[-1]')
+stl.success('Muons remaining at target distance:', n0[-1])
 
